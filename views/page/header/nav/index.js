11fc42af2d1e15b5c8aceea0bef8faa91579caf8
EdgarSalasV <edgardre01@gmail.com>
2020-08-20 14:17:32 -0500

    structure of getServerSideProps and styles of blog/id

--- views/page/header/nav/index.js
+++ views/page/header/nav/index.js
@@ -1,9 +1,23 @@
 import classes from './style.module.css'
-import { useContext, useState, useEffect } from 'react'
-import { AppContext } from '../../'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const LinkCustom = ({ href, children }) => {
+    const { pathname } = useRouter()
+    const classesLink = `
+        ${classes.link}
+        ${pathname===href ? classes.linkBold : ''}
+    `
+    return (
+        <Link href={href}>
+            <a className={classesLink}> { children } </a>
+        </Link>
+    )
+}
 
 const LinkHash = ({ id, children, ...props }) => {
-
+    const { pathname } = useRouter()
     const [selected, changeSelected] = useState(false)
 
     useEffect(() => {
@@ -11,11 +25,19 @@ const LinkHash = ({ id, children, ...props }) => {
             changeSelected(window.location.hash === '#' + id)
         })
     }, [])
-    return (
-        <a href={'#'+id} className={classes.link} style={{ 
-            fontWeight: selected ? 'bold' : 'normal',
-        }} {...props}> { children } </a>
-    )
+    if(pathname === '/') {
+        return (
+            <a href={'#'+id} className={classes.link} style={{ 
+                fontWeight: selected ? 'bold' : 'normal',
+            }} {...props}> { children } </a>
+        )
+    } else {
+        return (
+            <Link href={'/#' + id }>
+                <a className={classes.link} {...props}> { children } </a>
+            </Link>
+        )
+    }
 }
 
 const Nav = () => {
@@ -29,6 +51,7 @@ const Nav = () => {
                 <LinkHash id='what-we-do' onClick={() => setOpen(false)}> What we do </LinkHash>
                 <LinkHash id='who-we-are' onClick={() => setOpen(false)}> Who we are </LinkHash>
                 <LinkHash id='contact-us' onClick={() => setOpen(false)}> Contact us </LinkHash>
+                <LinkCustom href='/blog'> Blog </LinkCustom>
             </nav>
             <button className={classesOpen + ' ' + classesIcon}
                 onClick={()=>setOpen(!open)} > </button>
